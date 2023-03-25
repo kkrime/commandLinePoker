bdd3f0edcb8923225a3803b7ce594e554759ef8b
Iraq Jaber <IraqJaber@gmail.com>
2023-03-25 10:26:27 +0800

    fixup! inital commit

--- iceyePoker/poker/errors/errors.go
+++ iceyePoker/poker/errors/errors.go
@@ -1,7 +1,9 @@
+// package errors is used to store custom error types
 package errors
 
 import "fmt"
 
+// UserInputError is not a fatal error, but an error in the user input
 type UserInputError struct {
 	error string
 }
